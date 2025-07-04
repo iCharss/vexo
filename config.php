162b9b1d4f148fe0296584ab6cbfe2fa5239c964
iCharss <rdgz.carlos12@gmail.com>
2025-07-03 23:44:04 -0300

    Vexo

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'u307785954_vexo';
-$username = 'u307785954_vexo';
-$password = 'Vexo2025-'; 
+$dbname = '';
+$username = '';
+$password = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
